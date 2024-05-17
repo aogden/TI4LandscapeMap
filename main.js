dab60af714130ec0059b4ba15a937cbbab15ca49
Andrew Ogden <thisisandrewogden@gmail.com>
2024-05-17 14:56:48 -0700

    add button to switch orientation and slider to adjust the "split" of the map

--- main.js
+++ main.js
@@ -1,28 +1,83 @@
 const originalImage = document.getElementsByTagName("img")[0];
 const canvas = document.createElement('canvas');
-var context = canvas.getContext('2d');
-var imageObj = new Image();
+const context = canvas.getContext('2d');
+const imageObj = new Image();
+const gamebar = document.getElementsByClassName("gamebar")[0];
 
+var landscape = true;
+var splitPercentage = 0.5;
+
+// Get splitPercentage from localStorage
+splitPercentage = localStorage.getItem('splitPercentage');
+if (splitPercentage === null) {
+  splitPercentage = 0.5; // Default value
+} else {
+  splitPercentage = parseFloat(splitPercentage);
+}
+
+// Load the original image into the new image object
 originalImage.onload = function() {
     imageObj.src = originalImage.src;
     originalImage.parentElement.replaceChild(canvas, originalImage);
 };
 
+// Handle the loading of the image object
 imageObj.onload = function() {
-  var imgWidth = 2680;
-  var imgHeight = 4875;
-  var topHalfHeight = 2780;
-  var bottomHalfHeight = imgHeight - topHalfHeight;
-
-  canvas.width = imgWidth * 2;
-  canvas.height = topHalfHeight;
-
-  context.drawImage(
-    imageObj, 0, 0, imgWidth, topHalfHeight, 
-    0, 0, imgWidth, topHalfHeight
-  );
-  context.drawImage(
-    imageObj, 0, topHalfHeight, imgWidth, bottomHalfHeight, 
-    imgWidth, 0, imgWidth, bottomHalfHeight
-  );
+  drawImage();
 };
+
+// Function to draw the image in landscape orientation
+function drawImage() {
+  const imgWidth = originalImage.width;
+  const imgHeight = originalImage.height;
+  const topHalfHeight = imgHeight * splitPercentage;
+  const bottomHalfHeight = imgHeight - topHalfHeight;
+
+  if (landscape) {
+    canvas.width = imgWidth * 2;
+    canvas.height = topHalfHeight;
+
+    context.drawImage(
+      imageObj, 0, 0, imgWidth, topHalfHeight, 
+      0, 0, imgWidth, topHalfHeight
+    );
+    context.drawImage(
+      imageObj, 0, topHalfHeight, imgWidth, bottomHalfHeight, 
+      imgWidth, 0, imgWidth, bottomHalfHeight
+    );
+  } else {
+    canvas.width = imgWidth;
+    canvas.height = imgHeight;
+
+    context.drawImage(imageObj, 0, 0, imgWidth, imgHeight);
+  }
+}
+
+// Create and add the switch orientation button
+const switchOrientationButton = document.createElement('button');
+switchOrientationButton.innerText = 'Switch Orientation';
+gamebar.appendChild(switchOrientationButton);
+
+switchOrientationButton.addEventListener('click', function() {
+  landscape = !landscape;
+  drawImage();
+});
+
+// Create and add the split percentage label
+const splitPercentageLabel = document.createElement('span');
+splitPercentageLabel.textContent = 'Split %';
+gamebar.appendChild(splitPercentageLabel);
+
+// Create and add the split percentage slider
+const splitPercentageSlider = document.createElement('input');
+splitPercentageSlider.type = 'range';
+splitPercentageSlider.min = '0';
+splitPercentageSlider.max = '100';
+splitPercentageSlider.value = (splitPercentage * 100).toString();
+gamebar.appendChild(splitPercentageSlider);
+
+splitPercentageSlider.addEventListener('input', function() {
+  splitPercentage = splitPercentageSlider.value / 100;
+  localStorage.setItem('splitPercentage', splitPercentage.toString());
+  drawImage();
+});
